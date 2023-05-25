c25235aceda567b8a7ccd05a69d09b5ed4a44d5f
Anmol Git <Anmol>
2023-05-25 14:42:40 +0530

    test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let jsonserver = require("json-server");
 let server = jsonserver.create();
 let router = jsonserver.router("db.json");
 let midd = jsonserver.defaults();
+let cors = require("cors");
+server.use(cors());
 server.use(midd);
 server.use("", router);
 server.listen(8080, () => {
